22d9ea3756ea29456aeb8582f57cb179162635a7
Александр <palenko.as@mail.ru>
2022-07-26 12:37:51 +0300

    0. Spring Liquibase [#302330]

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -17,13 +17,3 @@ CREATE TABLE if not exists users (
      enabled boolean default true,
      authority_id int not null references authorities(id)
 );
-
-insert into posts (name) values ('О чем этот форум?');
-insert into posts (name) values ('Правила форума.');
-
-insert into authorities (authority) values ('ROLE_USER');
-insert into authorities (authority) values ('ROLE_ADMIN');
-
-insert into users (username, password, authority_id)
-values ('root', '$2a$10$4tgxeA6t3/mzum63x.11BODzpRTYbiN5Fg8fEzRNbrXSk56nN4ClS',
-        (select id from authorities where authority = 'ROLE_ADMIN'));
